d1cbad67a4747d1cc5ae2c601e3d0ce19992e3fc
Nick Williams <git@nickawilliams.com>
2025-01-18 13:40:46 -0800

    chore: reorganized things a bit, added some comment documentation

--- src/module.sh
+++ src/module.sh
@@ -1,73 +1,106 @@
-#!/usr/bin/env bash
+#!/usr/bin/env    bash
 
-module::route() {
+#?/name           module - a simple bash module system
+#?/synopsis       source module
+
+#?/environment    MODULE_DEBUG if set, debug information will be printed to the console
+#?/environment    MODULE_ARGS contains the arguments passed to the module
+
+set -euo pipefail
+
+#@/main
+ # Initializes the module system
+ #
+ # @arg $@ arguments to be forwarded to the module
+ #
+module() {
+  MODULE_ARGS=("$@")
+
+  trap 'module::_route' EXIT
+}
+
+#@/private
+# Applies rouing logic to <caller> (the sourcing script).
+#
+# @arg $@ arguments to be forwarded to the module
+#
+# @stdout debug information if MODULE_DEBUG is set
+# @stderr error message if the routing failed
+#
+# @exit 0 if the routing succeeded
+# @exit 1 if the routing failed
+#
+module::_route() {
   local module_name
 
-  # Only show debug information if MODULE_DEBUG is set
-  if [[ -n "${MODULE_DEBUG}" ]]; then
-    echo "Call stack:"
+  module::_debug "Function called with arguments: ${MODULE_ARGS[*]}"
+
+  if [[ "${MODULE_DEBUG:-0}" -eq 0 ]]; then
+    local call_stack="Call stack:"
     for ((i = 0; i < ${#FUNCNAME[@]}; i++)); do
-        echo "  [$i] ${FUNCNAME[$i]} (called from ${BASH_SOURCE[$i+1]}:${BASH_LINENO[$i]})"
+        if [[ -n ${BASH_SOURCE[$i+1]:-} ]]; then
+            call_stack+="\n  [$i] ${FUNCNAME[$i]} (called from ${BASH_SOURCE[$i+1]}:${BASH_LINENO[$i]})"
+        else
+            call_stack+="\n  [$i] ${FUNCNAME[$i]} (called from unknown location)"
+        fi
     done
-    echo "Function called with arguments: $*"
+
+    module::_debug "$call_stack"
   fi
 
-  # If parent script is being sourced, do nothing
-  if [[ "$(module::_check_execution_context)" == "sourced" ]]; then
+  # <caller> is being sourced, skip routing
+  if [[ "$(module::_check_context)" == "sourced" ]]; then
     return
   fi
 
-  # If parent script is being executed, perform routing:
+  # <caller> is being executed, apply routing
   module_name=$(module::_get_module_name "$0")
 
-  # if $1 is provided and  function <caller_file_without_extension>::$1() exists, run it and forward any arguments
-  if [[ -n "$1" ]] && declare -F "$module_name::$1" > /dev/null; then
+  # Route: <caller>::$1()
+  if [[ -n "${1:-}" ]] && declare -F "$module_name::$1" > /dev/null; then
     func="$module_name::$1"
     shift
-    "$func" "$@"
+    "$func" "${MODULE_ARGS[@]}"
     return
   fi
 
-  # otherwise if <caller_file_without_extension> exists, run it and forward any arguments
+  # Route: <caller>()
   if declare -F "$module_name" > /dev/null; then
-    "$module_name" "$@"
+    "$module_name" "${MODULE_ARGS[@]}"
     return
   fi
 
-  # otherwise, neither function exists
+  # Routing failed, exit with error
   echo "Error: Function '$module_name' not found."
   exit 1
 }
 
-module::_init() {
-  MODULE_ARGS=("$@")
-
-  trap 'module::_trap' EXIT
-}
-
-module::_trap() {
-  module::route "${MODULE_ARGS[@]}"
-}
-
-module::_get_module_name() {
-  filename=$(basename "$1")
-  echo "${filename%.*}"
-}
-
-module::_check_execution_context() {
+#@/private checks the execution context of the <caller> script
+module::_check_context() {
     # If module.sh is run directly, treat it as executed
-    if [[ "${#BASH_SOURCE[@]}" == 2 ]]; then  # Changed from 1 to 2 due to function
+    if [[ "${#BASH_SOURCE[@]}" == 2 ]]; then
         echo "executed"
         exit 0
     fi
 
     # Check if the calling script is being executed or sourced
-    # Using index 2 instead of 1 to skip the function's stack entry
-    if [[ "${BASH_SOURCE[3]}" == "$0" ]]; then
+    if [[ "${BASH_SOURCE[3]:-}" == "$0" ]]; then
         echo "executed"
     else
         echo "sourced"
     fi
 }
 
-module::_init "$@"
+#@/private prints debug information if MODULE_DEBUG is set
+module::_debug() {
+  [[ "${MODULE_DEBUG:-0}" -eq 0 ]] && return
+  echo "$*"
+}
+
+#@/private extracts the module name from the <caller> script
+module::_get_module_name() {
+  filename=$(basename "$1")
+  echo "${filename%.*}"
+}
+
+module "$@"
